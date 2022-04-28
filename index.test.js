3f76c6a115ab798e7030395be470e57c1d377318
Nicholas George <nicholas.george@greenclouds.in>
2022-04-28 12:57:48 +0530

    tests cleanup

--- index.test.js
+++ index.test.js
@@ -40,22 +40,3 @@ describe('Handling exceptions', () => {
     expect(inrWords('abc')).toEqual({ "error": "Not a number which can be converted." });
   });
 });
-// test('returns three hundred', () => {
-//   expect(inr(300))
-//     .toBe("three hundred rupee(s)");
-// });
-
-// test('returns five thousand', () => {
-//   expect(inr(5000))
-//     .toBe("five thousand rupee(s)");
-// });
-
-// test('returns one crore five', () => {
-//   expect(inr(10000005))
-//     .toBe("one crore five rupee(s)");
-// });
-
-// test('twenty four rupees and four paise', () => {
-//   expect(inr(24.04))
-//     .toBe("twenty four rupee(s) and four paise");
-// });
